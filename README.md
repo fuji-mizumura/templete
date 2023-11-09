aca42fd33e064292372baebf36f40c6ec3d92635
fuji-mizumura <shuicunyoujin@gmail.com>
2023-11-09 15:29:34 +0900

    8.2 「api」のコード見直しと「api」から「db」へアクセスしてデータ取得

--- README.md
+++ README.md
@@ -11,22 +11,22 @@
 
 # 作成手順
 
-1.  Git 管理
+1.  **Git 管理**
     1. `git init`
     2. `git add .`
     3. `git commit -m "first commit"`
     4. GitHub でリモートリポジトリの作成
     5. `git remote add origin https://github.com/fuji-mizumura/templete.git`
     6. `git push -u origin main`
-2.  ネットワークを作成
+2.  **ネットワークを作成**
     1. `docker network create shared-net`
-3.  「server」コンテナの作成
+3.  **「server」コンテナの作成**
     1. docker-compose.yml, コンテナ用ディレクトリ, Dockerfile を作成
     2. `docker compose up -d`
     3. `docker compose cp server:/usr/local/apache2/conf/httpd.conf ./server`コンテナ内の httpd.conf をホストの server ディレクトリにコピー
     4. httpd.conf をマウントさせるよう docker-compose.yml に volumes の記述を追加
     5. localhost（ポート指定なし）で動作を確認
-4.  「front」コンテナの作成
+4.  **「front」コンテナの作成**
     1. docker-compose.yml に追記, コンテナ用ディレクトリ, Dockerfile を作成
     2. `docker compose up -d`
     3. `docker compose exec front ash`
@@ -34,14 +34,23 @@
     5. .gitignore を作成、package.json に scripts を追記、app/page.tsx、app/layout.tsx を作成
     6. `npm run dev`で起動し、localhost:3000 でのレスポンスを確認
     7. Dockerfile に`CMD [ "npm","run","dev" ]`を追記
-5.  「api」コンテナの作成
+5.  **「api」コンテナの作成**
     1. docker-compose.yml に追記, コンテナ用ディレクトリ, Dockerfile を作成
     2. ※今回は前回のテストで使用した Flask の api ディレクトリを丸ごとコピーした
     3. localhost:5000 でのレスポンスを確認
-6.  「db」コンテナの作成
+6.  **「db」コンテナの作成**
     1. docker-compose.yml に追記, コンテナ用ディレクトリ, Dockerfile を作成
     2. .gitignore の作成
-7. httpd.confに設定
+7. **httpd.confを修正**
+   1. 142行目mod_proxy.soをアクティブ化
+   2. 145行目mod_proxy_http.soをアクティブ化
+   3. 241行目ServerNameの変更（今回はlocalhostにした）
+   4. 265~292行目DocumentRoot関連の全ての内容をコメントアウト
+   5. 552行目以降にリバースプロキシの設定を記述
+   6. コンテナを再起動か`httpd -k restart`で設定を反映させ、localhostとlocalhost/apiにアクセスしてレスポンスを確認
+8. 「api」から「db」への疎通確認
+   1. データベースの環境変数を「api」内のコードとして反映、またそれに伴ってコードを修正
+   2. 「api」から「db」へアクセスしてデータ取得を確認
 
 # 目標
 
--- api/app/app.py
+++ api/app/app.py
@@ -1,40 +1,110 @@
 import json
-from flask import Flask
+from flask import Flask, render_template
 from flask_cors import CORS
-# import MySQLdb
+import psycopg2
+import psycopg2.extras
+
 
 app = Flask(__name__)
 CORS(app)
 
-# API
 
+# ルートにアクセスした際の処理
 @app.route('/')
-def test_api():
-    return json.dumps({
-        'message':'thanks for testing the api',
-    })
+def hello():
+    return 'Hello World!'
 
-# @app.route('/db')
-# def db_api():
-#     connection = MySQLdb.connect(
-#         host='mysql',
-#         user='project',
-#         passwd='project',
-#         db='users')
-#     cursor = connection.cursor()
 
-    cursor.execute("""SHOW CREATE DATABASE test_database;
-    """)
+# DBに接続して従業員リストをテンプレートを用いて表示
+@app.route('/db')
+def index():
+    try:
+        # PostgreSQL Serverとの接続を定義し、接続を作成
+        conn = psycopg2.connect(
+            host='db',
+            port=5432,
+            database='postgres',
+            user='postgres',
+            password='postgres',
+        )
+        cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
 
-    myresult = cursor.fetchall()
+        # SQLを投入して情報取得
+        cur.execute('select * from employee order by employee_id')
+        employee_list = cur.fetchall()
 
-    for row in myresult:
-        print(row)
+        # データをhtmlに渡して表示
+        return render_template(
+            'app_template.html',
+            title='Flask PostgreSQL',
+            dataset=employee_list
+        )
+    except psycopg2.errors:
+        return json.dumps({
+            'message':'error',
+        })
+        pass
 
-    connection.close()
-    return json.dumps({
-        'message':'thanks for db testing',
-    })
+    # 接続終了
+    conn.close()
+
+
+# DBに接続して従業員リストを作成
+@app.route('/db/reset')
+def db_api():
+    try:
+        # PostgreSQL Serverとの接続を定義し、接続を作成
+        conn = psycopg2.connect(
+            host='db',
+            port=5432,
+            database='postgres',
+            user='postgres',
+            password='postgres',
+        )
+        cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+
+        # テーブルが存在するか確認し、存在しない場合は作成
+        cur.execute("SELECT EXISTS(SELECT relname FROM pg_class WHERE relname='employee')")
+        table_exists = cur.fetchone()[0]
+        if not(table_exists):
+            cur.execute('create table employee (employee_id int, employee_name varchar(255), employee_age int)')
+            conn.commit()
+            employee_list = []
+
+        # テーブルにデータが存在する場合は削除
+        cur.execute('select * from employee')
+        employee_list = cur.fetchall()
+        if len(employee_list) > 0:
+            cur.execute('delete from employee')
+            conn.commit()
+
+        # 10件分のダミーデータを作成
+        cur.execute("insert into employee values (1, 'Taro Yamada', 20)")
+        cur.execute("insert into employee values (2, 'Hanako Yamada', 22)")
+        cur.execute("insert into employee values (3, 'Jiro Yamada', 25)")
+        cur.execute("insert into employee values (4, 'Sachiko Yamada', 27)")
+        cur.execute("insert into employee values (5, 'Saburo Yamada', 30)")
+        cur.execute("insert into employee values (6, 'Shizuka Yamada', 33)")
+        cur.execute("insert into employee values (7, 'Goro Yamada', 35)")
+        cur.execute("insert into employee values (8, 'Miki Yamada', 37)")
+        cur.execute("insert into employee values (9, 'Ichiro Yamada', 40)")
+        cur.execute("insert into employee values (10, 'Junko Yamada', 42)")
+
+        # コミット
+        conn.commit()
+
+        return json.dumps({
+            'message':'ダミーデータを作成しました',
+        })
+
+    except psycopg2.errors:
+        return json.dumps({
+            'message':'error',
+        })
+        pass
+
+    # 接続終了
+    conn.close()
 
 
 app.run()
